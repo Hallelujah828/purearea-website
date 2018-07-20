f424a38b15927b9e52ffd6b8584bd69bf0b4ea80
Hallelujah828 <1105725109@qq.com>
2018-07-20 11:33:42 +0800

    up

--- config/index.js
+++ config/index.js
@@ -49,7 +49,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: 'https://purearea.oss-cn-shanghai.aliyuncs.com/',
 
     /**
      * Source Maps
